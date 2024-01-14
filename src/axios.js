dc23615db491370ee936f03bfd30ca52b2ff2efe
balaji-2711 <smilewithbalaa@gmail.com>
2024-01-14 14:37:52 +0530

    second commit

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 //backend base url
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://whatsappb-4w5r.onrender.com",
 });
 
 export default instance;
